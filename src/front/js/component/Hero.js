bbf6a1b739172473d584e94ae3e4f5dafbe1d21b
Hguillenalvarado <heinerguillen@gmail.com>
2021-04-26 02:27:37 +0000

    organización componentes de home

---
+++ src/front/js/component/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Hero() {
+	return (
+		<div className="jumbotron">
+			<h1 className="display-3">Busc@PYME</h1>
+			<p className="led">Aquí falta definir el texto</p>
+			<hr className="my-4" />
+			<p className="lead">
+				<a className="btn btn-primary btn-lg" href="#" role="button">
+					Botón Acción
+				</a>
+			</p>
+		</div>
+	);
+}
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,16 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../img/rigo-baby.jpg";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<a className="navbar-brand" href="#">
+				<img className="w-25" src={logo} />
+			</a>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarColor02"
+				aria-controls="navbarColor02"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon" />
+			</button>
+
+			<div className="collapse navbar-collapse" id="navbarColor02">
+				<ul className="navbar-nav mr-auto">
+					<li className="nav-item active">
+						<a className="nav-link" href="#">
+							Inicio
+							<span className="sr-only">(current)</span>
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Registro
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Login
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Provincias
+						</a>
+					</li>
+				</ul>
+				<form className="form-inline my-2 my-lg-0">
+					<input className="form-control mr-sm-2" type="text" placeholder="Search" />
+					<button className="btn btn-secondary my-2 my-sm-0" type="submit">
+						Search
+					</button>
+				</form>
 			</div>
 		</nav>
 	);
