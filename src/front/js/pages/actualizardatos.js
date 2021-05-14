f2d0e988ce9d26aea68da939c94e1dbb04cfeaa2
Julio Avila <hellboy2015@gmail.com>
2021-05-14 00:20:26 +0000

    fixed actualizardatos y registropymes

--- src/front/js/pages/actualizardatos.js
+++ src/front/js/pages/actualizardatos.js
@@ -3,94 +3,154 @@ import { Context } from "../store/appContext";
 import "../../styles/registropymes.scss";
 import { Home } from "./home";
 
-export const Actualizardatos = actualizardatos => {
+export const Actualizardatos = () => {
 	const { store, actions } = useContext(Context);
-
 	const [provinciaId, setProvinciaId] = useState(1);
-	const handleSelect = e => {
-		setProvinciaId(parseInt(e.target.value));
+	const [canton, setCanton] = useState(1);
+	const [nombrePyme, setNombrePyme] = useState("");
+	const [tipoServicio, setTipoServicio] = useState(1);
+	const [telefono, setTelefono] = useState("");
+	const [otrasSenas, setOtrasSenas] = useState("");
+	const [instagram, setInstagram] = useState("");
+	const [facebook, setFacebook] = useState("");
+	const [token, setToken] = useState(sessionStorage.getItem("my_token"));
+
+	/* const handleSelect = e => {
+        setProvinciaId(parseInt(e.target.value));
+    }; */
+
+	const handleActualizarDatos = e => {
+		e.preventDefault();
+
+		if (!token) {
+			return;
+		}
+
+		const body = {
+			provincia: provinciaId,
+			canton: provinciaId,
+			nombrePyme: nombrePyme,
+			tipoServicio: tipoServicio,
+			telefono: telefono,
+			otrasSenas: otrasSenas,
+			instagram: instagram,
+			facebook: facebook
+		};
+		var requestOptions = {
+			method: "POST",
+			body: JSON.stringify(body),
+			redirect: "follow",
+			headers: new Headers({
+				"Content-Type": "application/json",
+				Accept: "*/*",
+				Authorization: "Bearer " + token
+			})
+		};
+
+		fetch("https://busca-pyme.herokuapp.com/api/actualizapyme", requestOptions)
+			.then(response => response.json())
+			.then(result => {
+				console.log(result);
+				setStore({
+					favorites: result
+				});
+			})
+			.catch(error => console.log("error", error));
 	};
-	/* useEffect(() => {
-    actions.loadProvincias();
-    actions.loadCantones();
-    actions.loadServicios();
-}, []); */
+
 	return (
 		<div className="container">
 			<div className="wrapper text-white">
-				<div className="form-provincia">
-					<label form="sel1">Provincia:</label>
-					<select className="form-control" id="sel1" onChange={handleSelect}>
-						{store.provincias.map(item => (
-							<option key={item.id} value={item.id}>
-								{item.nombre}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="form-Cantón">
-					<label form="usr">Cantón:</label>
-					<select className="form-control" id="sel1">
-						{store.cantones
-							.filter(cantonesToFilter => cantonesToFilter.id_provincias === provinciaId)
-							.map((item, index) => (
-								<option key={index} value={item.id}>
+				<form onSubmit={handleActualizarDatos} style={{ width: "500px" }}>
+					<div className="form-provincia">
+						<label form="sel1">Provincia:</label>
+						<select
+							className="form-control"
+							id="sel1"
+							onChange={e => setProvinciaId(parseInt(e.target.value))}>
+							{store.provincias.map(item => (
+								<option key={item.id} value={item.id}>
 									{item.nombre}
 								</option>
 							))}
-					</select>
-				</div>
-				<div className="form-Name">
-					<label form="usr">Nombre Pymes:</label>
-					<input type="text" className="form-control" id="usr" />
-				</div>
-				<div className="form-servicios">
-					<label form="sel1">Servicios:</label>
-					<select className="form-control" id="sel1">
-						{store.servicios.map(item => (
-							<option key={item.id} value={item.id}>
-								{item.tipo}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className="form-Teléfono">
-					<label form="usr">Teléfono:</label>
-					<input type="text" className="form-control" id="usr" />
-				</div>
-				<div className="form-Teléfono">
-					<label form="usr">Otras Señas:</label>
-					<input type="text" className="form-control" id="usr" />
-				</div>
-				<div className="form-Instagram">
-					<label form="usr">Instagram:</label>
-					<input type="text" className="form-control" id="usr" />
-				</div>
-				<div className="form-Facebook">
-					<label form="usr">Facebook:</label>
-					<input type="text" className="form-control" id="usr" />
-				</div>
-				<div>
-					<select name="select">
-						<option value="https://images.freejpg.com.ar/900/0405/clothes-jackets-shirts-fashion-clothing-shopping-retail-F100024217.jpg">
-							Ropa
-						</option>
-						<option value="https://images.freejpg.com.ar/900/0705/fork-chicken-tomatoes-pasta-food-lunch-dinner-F100025310.jpg">
-							Comida
-						</option>
-						<option value="https://www.freejpg.com.ar/image-900/dd/dd33/F100011987-mecanico_posando_en_un_taller_mecanico.jpg">
-							Servicios
-						</option>
-						<option value="https://images.freejpg.com.ar/900/0405/people-meeting-computer-business-coworkers-wireless-technology-F100024601.jpg">
-							Otros
-						</option>
-					</select>
-				</div>
-				<div className="col text-center">
-					<button type="submit" className=" spinner">
-						Ingresar PYME
-					</button>
-				</div>
+						</select>
+					</div>
+					<div className="form-Cantón">
+						<label form="usr">Cantón:</label>
+						<select className="form-control" id="sel1" onChange={e => setCanton(parseInt(e.target.value))}>
+							{store.cantones
+								.filter(cantonesToFilter => cantonesToFilter.id_provincias === provinciaId)
+								.map((item, index) => (
+									<option key={index} value={item.id}>
+										{item.nombre}
+									</option>
+								))}
+						</select>
+					</div>
+					<div className="form-Name">
+						<label form="usr">Nombre Pymes:</label>
+						<input
+							type="text"
+							className="form-control"
+							id="nombrePyme"
+							onChange={e => setNombrePyme(e.target.value)}
+						/>
+					</div>
+					<div className="form-servicios">
+						<label form="sel1">Servicios:</label>
+						<select
+							className="form-control"
+							id="servicios"
+							onChange={e => setTipoServicio(parseInt(e.target.value))}>
+							{store.servicios.map(item => (
+								<option key={item.id} value={item.id}>
+									{item.tipo}
+								</option>
+							))}
+						</select>
+					</div>
+					<div className="form-Teléfono">
+						<label form="telefono">Teléfono:</label>
+						<input
+							type="text"
+							className="form-control"
+							id="telefono"
+							onChange={e => setTelefono(e.target.value)}
+						/>
+					</div>
+					<div className="form-Teléfono">
+						<label form="otrasSenas">Otras Señas:</label>
+						<input
+							type="text"
+							className="form-control"
+							id="otrasSenas"
+							onChange={e => setOtrasSenas(e.target.value)}
+						/>
+					</div>
+					<div className="form-Instagram">
+						<label form="instagram">Instagram:</label>
+						<input
+							type="text"
+							className="form-control"
+							id="instagram"
+							onChange={e => setInstagram(e.target.value)}
+						/>
+					</div>
+					<div className="form-Facebook">
+						<label form="facebook">Facebook:</label>
+						<input
+							type="text"
+							className="form-control"
+							id="facebook"
+							onChange={e => setFacebook(e.target.value)}
+						/>
+					</div>
+					<div className="col text-center">
+						<button type="submit" className=" spinner">
+							Ingresar PYME
+						</button>
+					</div>
+				</form>
 			</div>
 		</div>
 	);
