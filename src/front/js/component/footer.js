913b34dad4c927b0fc24be1840318642bd5a7c41
Julio Avila <hellboy2015@gmail.com>
2021-05-18 05:49:43 +0000

    added mailto in footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center text-white bg-dark">
 		<p>
-			Contacto <a href="http://www.google.com">buscapymes@gmail.com</a>
+			Contacto <a href="mailto:buscapymes@gmail.com">buscapymes@gmail.com</a>
 		</p>
 	</footer>
 );
